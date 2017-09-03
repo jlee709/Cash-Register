51e9df285dbe332b9f183ee831ba6cf799955ea1
Justin Wayne Lee <justinlee709@amsesm.cnm7103.co.kr>
2017-09-03 18:46:12 +0900

    added more notes to design doc

---
+++ js/cash_register.js
@@ -0,0 +1,37 @@
+// logic of the cash register
+
+// main points 
+
+// 1. ( display ) is the primary ui that prints all results
+
+// 2. [clear] will clear the display
+
+// 3. [get balance] will display the current balance
+
+// 4. [deposit cash] will add the amount currently in the ( display ) to the cash register, then clears the display
+
+// 5. [withdraw cash] will remove the amount currently in the ( display ) to the cash register, then clears the display
+
+
+// First set up design layout
+
+
+
+// have acsess to data in the html files via the dom
+
+
+
+// use the DOM to get acsess to each key 
+
+
+//apply the calculator function from cal file to the cash register logic
+
+
+// $$ Check to see that onClick value is taken and can be passed into
+// $$ the input field for the operations !!! make cash register work
+// $$ with basic integers first then refactor for floats
+
+
+
+
+// * parse ints, join method and for each loops and arrays is a way to use * 
